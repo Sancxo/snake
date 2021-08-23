c164f64fad4baa631630376c1b10a8cc26c63a1e
Simon <simon@7lieues.io>
2021-08-23 16:41:01 +0200

    PWA creation

---
+++ sw.js
@@ -0,0 +1,30 @@
+let cacheName = "snake-game-page";
+let filesToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js'
+];
+
+self.addEventListener('install', e => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            console.log('[Service Worker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request, {ignoreSearch: true})
+        .then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
