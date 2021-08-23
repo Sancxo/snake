37b68f3344d08ba8706f3f50de336e5e47630da8
Simon <simon@7lieues.io>
2021-08-23 18:30:09 +0200

    service worker cleaning

--- sw.js
+++ sw.js
@@ -7,10 +7,10 @@ let filesToCache = [
 ];
 
 self.addEventListener('install', e => {
-    console.log('[Service Worker] Install');
+    // console.log('[Service Worker] Install');
     e.waitUntil(
         caches.open(cacheName).then(cache => {
-            console.log('[Service Worker] Caching app shell');
+            // console.log('[Service Worker] Caching app shell');
             return cache.addAll(filesToCache);
         })
     );
